24f026e85b53c0723bf6a323dbd0b2941a6a8958
Lynn McKay <iamlynnmckay@gmail.com>
2022-02-25 12:30:39 -0800

    Fri Feb 25 12:30:39 PST 2022

--- main.sh
+++ main.sh
@@ -2,22 +2,19 @@
 
 set -x -o pipefail
 
-_Dotfiles_clean() {
-    local -r _tempDir="${HOME}/.backup/dotfiles/$(date +%s)${RANDOM}"
-    mkdir -p "${_tempDir}"
-	mv "${HOME}/.bashrc" "${_tempDir}" || :
-	mv "${HOME}/.config/nvim/init.vim" "${_tempDir}" || :
-	mv "${HOME}/.gitignore" "${_tempDir}" || :
-	mv "${HOME}/.ideavimrc" "${_tempDir}" || :
-	mv "${HOME}/.ohmyzshrc" "${_tempDir}" || :
-	mv "${HOME}/.profile" "${_tempDir}" || :
-	mv "${HOME}/.public" "${_tempDir}" || :
-	mv "${HOME}/.vimrc" "${_tempDir}" || :
-	mv "${HOME}/.zprofile" "${_tempDir}" || :
-	mv "${HOME}/.zshrc" "${_tempDir}" || :
+
+_Dotfiles___copy() {
+    local -r _src="${1:?}"
+    local -r _dest="${2:?}"
+	cp -rf "${_src}" "${_dest}"
 }
 
-_Dotfiles_install() {
+_Dotfiles___remove() {
+    local -r _dest="${1:?}"
+	rm -rf "${_dest}"
+}
+
+_Dotfiles___link() {
     local -r _root="${1:?}"
 	mkdir -p "${HOME}/.config/nvim"
 	ln -sf "${_root}/public" "${HOME}/.public"
@@ -32,13 +29,46 @@ _Dotfiles_install() {
 	ln -sf "${_root}/zshrc" "${HOME}/.zshrc"
 }
 
+_Dotfiles___unlink() {
+    local -r _tempDir="${HOME}/.backup/dotfiles/$(date +%s)${RANDOM}"
+    mkdir -p "${_tempDir}"
+	mv "${HOME}/.bashrc" "${_tempDir}" || :
+	mv "${HOME}/.config/nvim/init.vim" "${_tempDir}" || :
+	mv "${HOME}/.gitignore" "${_tempDir}" || :
+	mv "${HOME}/.ideavimrc" "${_tempDir}" || :
+	mv "${HOME}/.ohmyzshrc" "${_tempDir}" || :
+	mv "${HOME}/.profile" "${_tempDir}" || :
+	mv "${HOME}/.public" "${_tempDir}" || :
+	mv "${HOME}/.vimrc" "${_tempDir}" || :
+	mv "${HOME}/.zprofile" "${_tempDir}" || :
+	mv "${HOME}/.zshrc" "${_tempDir}" || :
+}
+
+_Dotfiles_homebrew() {
+	_Dotfiles___link "$(readlink -f /usr/local/opt/dotfiles)"
+}
+
+_Dotfiles_install() {
+	local -r _src="${PWD}/src"
+	local -r _dest="${HOME}/.dotfiles"
+	_Dotfiles_clean "${_dest}"
+	_Dotfiles___copy "${_src}" "${_dest}"
+	_Dotfiles___link "${_dest}"
+}
+
+_Dotfiles_clean() {
+	local -r _dest="${PWD}/.dotfiles"
+	_Dotfiles___unlink
+	_Dotfiles___remove "${_dest}"
+}
+
 ___main___() {
     case "${1}" in
         "--install")
-            _Dotfiles_install "${PWD}/src"
+            _Dotfiles_install
         ;;
 		"--homebrew")
-			_Dotfiles_install "$(readlink -f /usr/local/opt/dotfiles)"
+			_Dotfiles_homebrew
 		;;
         "--clean")
             _Dotfiles_clean 
